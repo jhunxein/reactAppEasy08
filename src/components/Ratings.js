537c9faa4a010c202665baa3c60f4bba7c407bb4
jhunxein <jhunsensamonte@gmail.com>
2021-03-31 20:42:36 -0800

    Reverting img to div

--- src/components/Ratings.js
+++ src/components/Ratings.js
@@ -1,13 +1,11 @@
 function Ratings({ name, rate }) {
 	return (
 		<figure>
-			<img
-				src="/"
-				alt="star rating"
+			<div
 				className="stars"
 				style={{ '--rating': rate }}
 				aria-label={`${rate > 1 ? rate + ' star' : rate + ' stars'} out of 5`}
-			/>
+			></div>
 			<figcaption>Rated 5 stars in {name}</figcaption>
 		</figure>
 	);
